12fbf9a924cd67ac596ce0811a85b76b35cc5cc8
Isnadia52 <isnadiyahnurfadhilah@gmail.com>
2025-05-10 23:08:13 +0700

    melengkapi method

--- src/movie/Movie.java
+++ src/movie/Movie.java
@@ -1,15 +1,69 @@
-package movie;
+// package movie;
+
+// import java.util.ArrayList;
+// import java.util.List;
+
+// public class Movie {
+//     private String title;
+//     private String genre;
+//     private int duration;
+//     private List<Schedule> schedules;
+
+//     public Movie(String title, String genre, int duration) {
+//         this.title = title;
+//         this.genre = genre;
+//         this.duration = duration;
+//         this.schedules = new ArrayList<>();
+//     }
+
+//     public List<Schedule> getSchedules() {
+//         return schedules;
+//     }
+
+//     public void addSchedule(Schedule schedule) {
+//         // TODO: Implement adding schedule logic
+//     }
+
+//     @Override
+//     public String toString() {
+//         // TODO: Implement toString method
+//         return null;
+//     }
+
+//     // Getters and Setters
+//     public String getTitle() {
+//         return title;
+//     }
+
+//     public void setTitle(String title) {
+//         this.title = title;
+//     }
+
+//     public String getGenre() {
+//         return genre;
+//     }
+
+//     public void setGenre(String genre) {
+//         this.genre = genre;
+//     }
+
+//     public int getDuration() {
+//         return duration;
+//     }
+
+//     public void setDuration(int duration) {
+//         this.duration = duration;
+//     }
+// }
 
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Movie {
     private String title;
     private String genre;
-    private int duration;
+    private int duration; // in minutes
     private List<Schedule> schedules;
-    
 
     public Movie(String title, String genre, int duration) {
         this.title = title;
@@ -17,46 +71,33 @@ public class Movie {
         this.duration = duration;
         this.schedules = new ArrayList<>();
     }
-    
+
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
 
     public List<Schedule> getSchedules() {
         return schedules;
     }
-    
 
+    // Add a schedule
     public void addSchedule(Schedule schedule) {
-        // TODO: Implement adding schedule logic
+        if (schedule != null) {
+            schedules.add(schedule);
+        }
     }
-    
 
     @Override
     public String toString() {
-        // TODO: Implement toString method
-        return null;
-    }
-    
-    // Getters and Setters
-    public String getTitle() {
-        return title;
-    }
-    
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    
-    public String getGenre() {
-        return genre;
-    }
-    
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-    
-    public int getDuration() {
-        return duration;
-    }
-    
-    public void setDuration(int duration) {
-        this.duration = duration;
+        return title + " (" + genre + ", " + duration + " mins)";
     }
 }
--- src/movie/Schedule.java
+++ src/movie/Schedule.java
@@ -1,1 +1,49 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Schedule {
+    private Movie movie;
+    private LocalDateTime dateTime;
+    private double price;
+    private int availableSeats;
+
+    public Schedule(Movie movie, LocalDateTime dateTime, double price, int availableSeats) {
+        this.movie = movie;
+        this.dateTime = dateTime;
+        this.price = price;
+        this.availableSeats = availableSeats;
+    }
+
+    // Getters
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public boolean bookSeat() {
+        if (availableSeats > 0) {
+            availableSeats--;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm");
+        return movie.getTitle() + " - " +
+               dateTime.format(formatter) + " | Rp" + price + " | Seats: " + availableSeats;
+    }
+}
 
