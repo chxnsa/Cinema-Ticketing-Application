c571fc9986a16b7e6344aff2193cdd0c0083b74f
unknown <azzahrasyam49@gmail.com>
2025-05-12 21:43:54 +0800

    memperbaiki class reservation

--- src/reservation/Reservation.java
+++ src/reservation/Reservation.java
@@ -1,62 +1,32 @@
 package reservation;
 
-import user.Customer;
+import users.Customer;
 import movie.Schedule;
 
-/**
- * Class representing a Reservation in the Cinema Ticketing System
- */
 public class Reservation {
-    // Fields
     private Customer customer;
     private Schedule schedule;
     private int seatNumber;
     private double totalPrice;
-    
-    /**
-     * Constructor for Reservation
-     * @param customer the customer making reservation
-     * @param schedule the movie schedule
-     * @param seatNumber the seat number
-     */
+
     public Reservation(Customer customer, Schedule schedule, int seatNumber) {
         this.customer = customer;
         this.schedule = schedule;
         this.seatNumber = seatNumber;
-        this.totalPrice = schedule.getPrice(); // Basic price, might be modified later
+        this.totalPrice = schedule.getPrice();
     }
-    
-    /**
-     * Print information about this reservation
-     */
+
     public void printInfo() {
-        System.out.println("===== Reservation Info =====");
-        System.out.println("Customer Name: " + customer.getName());
-        System.out.println("Movie Title: " + schedule.getMovie().getTitle());
-        System.out.println("Showtime: " + schedule.getTime());
-        System.out.println("Seat Number: " + seatNumber);
-        System.out.println("Total Price: $" + totalPrice);
-        System.out.println("============================");
+        System.out.println("\n===============================");
+        System.out.println("TIKET BIOSKOP");
+        System.out.println("===============================");
+        System.out.println("Nama: " + customer.getUsername());
+        System.out.println("Film: " + schedule.getMovie().getTitle());
+        System.out.println("Tanggal: " + schedule.getDateTime());
+        System.out.println("Nomor Kursi: " + seatNumber);
+        System.out.println("Harga: Rp " + String.format("%,.0f", totalPrice));
+        System.out.println("===============================");
+        System.out.println("Terima kasih & selamat menonton!");
     }
-    
-    // Getters and Setters
-    public Customer getCustomer() {
-        return customer;
-    }
-    
-    public Schedule getSchedule() {
-        return schedule;
-    }
-    
-    public int getSeatNumber() {
-        return seatNumber;
-    }
-    
-    public double getTotalPrice() {
-        return totalPrice;
-    }
-    
-    public void setTotalPrice(double totalPrice) {
-        this.totalPrice = totalPrice;
-    }
+
 }
